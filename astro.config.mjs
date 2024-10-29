4f4e1b0b09e0c7351301d4177af9864463692ef0
akukhan <akukhan19900@gmail.com>
2024-10-29 05:50:39 +0530

    remove workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://akukhan.github.io',
-    
-});
+export default defineConfig({});
